f3fdddbe702654d06426360ed3f74a200050594f
Muhammad Alam <malam53345@gmail.com>
2023-10-25 23:20:33 +0500

    home page completed

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <span>M Alam</span>
+            <span>© M Alam Admin Dashboard</span>
+        </div>
+    )
+}
+
+export default Footer
